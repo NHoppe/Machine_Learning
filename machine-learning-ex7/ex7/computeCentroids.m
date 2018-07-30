2a9749c40063b354f86ac3d3678effa644e1740a
Natan Hoppe <nhoppe@gmail.com>
2018-07-30 15:18:14 -0700

    Checkpoint exercise 7

---
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -0,0 +1,41 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+% This is a vectorized solution
+vectorized_selection = (idx == (1:K));
+vectorized_sum = vectorized_selection' * X;
+vectorized_examples = sum(vectorized_selection, 1);
+
+% Divide vectorized_sum (3x2) by vectorized_examples (1x3)
+% (element wise operation for each column of vectorized_examples)
+centroids = bsxfun(@rdivide, vectorized_sum, vectorized_examples');
+
+% =============================================================
+
+end
+
